657208f85be7dfd3d4332a41b5c91a20c593bd99
Holliak Sofia <holliak.sofia@gmail.com>
2024-12-24 13:54:19 +0200

    Complete

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://sofia:12345z@cluster0.o2rcx.mongodb.net/bdDev?retryWrites=true&w=majority&appName=Cluster0',
+  secretOrKey: 'random very very secret string',
+};
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const globalConfigs = require('./routes/globalConfigs');
+const users = require('./routes/user');
+const posts = require('./routes/post');
+const comments = require('./routes/comments');
+const awards = require('./routes/awards');
+// const mainRoute = require('./routes/index');
+
+const app = express();
+
+const allowedOrigins = ['http://localhost:5173', 'https://fitness-app-delta-six.vercel.app'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useFindAndModify: false ,  useUnifiedTopology: true,})
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/configs', globalConfigs);
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/comments', comments);
+app.use('/api/awards', awards);
+// app.use('/', mainRoute);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
