6a34ee822ded2a1415a5caa24905124544cca8c3
Holliak Sofia <holliak.sofia@gmail.com>
2024-12-26 13:30:46 +0200

    complete

--- server.js
+++ server.js
@@ -15,32 +15,25 @@ const awards = require('./routes/awards');
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173', 'https://fitness-f.vercel.app/',  'https://fitnessback-production.up.railway.app'];
+const allowedOrigins = ['http://localhost:5173', 'https://fitness-f.vercel.app',  'https://fitnessback-production.up.railway.app'];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200,
+  credentials: true, // Дозволяє cookie/авторизаційні заголовки
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // Заголовки
+  optionsSuccessStatus: 200, // Для preflight
 };
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Дозволити всі джерела
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-app.options('*', cors(corsOptions)); // Дозволяє preflight-запити
-
 app.use(cors(corsOptions));
+
+app.options('*', cors(corsOptions));
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
